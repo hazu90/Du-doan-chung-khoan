17b06f48f6b170db0c896bc9782a8412c9948183
hieubv <hieubv.cntt@gmail.com>
2018-11-22 17:50:50 +0700

    commit from my office

--- Du doan tai chinh/content/js-ext/main-ext.js
+++ Du doan tai chinh/content/js-ext/main-ext.js
@@ -1,21 +1,95 @@
 var global_key ={
-    nine_frame_key_du_doan :'nine_frame_key_predict',
-    nine_frame_1st_frame_ttt_key :'nine_frame_1st_frame_ttt',
-    nine_frame_1st_frame_ttf_key :'nine_frame_1st_frame_ttf',
-    nine_frame_1st_frame_tft_key :'nine_frame_1st_frame_tft',
-    nine_frame_1st_frame_tff_key :'nine_frame_1st_frame_tff',
-    nine_frame_1st_frame_ftt_key :'nine_frame_1st_frame_ftt',
-    nine_frame_1st_frame_ftf_key :'nine_frame_1st_frame_ftf',
-    nine_frame_1st_frame_fft_key :'nine_frame_1st_frame_fft',
-    nine_frame_1st_frame_fff_key :'nine_frame_1st_frame_fff',
-    nine_frame_2st_frame_ttt_key :'nine_frame_2st_frame_ttt',
-    nine_frame_2st_frame_ttf_key :'nine_frame_2st_frame_ttf',
-    nine_frame_2st_frame_tft_key :'nine_frame_2st_frame_tft',
-    nine_frame_2st_frame_tff_key :'nine_frame_2st_frame_tff',
-    nine_frame_2st_frame_ftt_key :'nine_frame_2st_frame_ftt',
-    nine_frame_2st_frame_ftf_key :'nine_frame_2st_frame_ftf',
-    nine_frame_2st_frame_fft_key :'nine_frame_2st_frame_fft',
-    nine_frame_2st_frame_fff_key :'nine_frame_2st_frame_fff',   
+    nine_frame_key_du_doan: 'nine_frame_key_predict',
+    nine_frame_1st_frame: {
+        ttt_key: 'nine_frame_1st_frame_ttt',
+        ttf_key: 'nine_frame_1st_frame_ttf',
+        tft_key: 'nine_frame_1st_frame_tft',
+        tff_key: 'nine_frame_1st_frame_tff',
+        ftt_key: 'nine_frame_1st_frame_ftt',
+        ftf_key: 'nine_frame_1st_frame_ftf',
+        fft_key: 'nine_frame_1st_frame_fft',
+        fff_key: 'nine_frame_1st_frame_fff'
+    },
+    nine_frame_2st_frame: {
+        ttt_key: 'nine_frame_2st_frame_ttt',
+        ttf_key: 'nine_frame_2st_frame_ttf',
+        tft_key: 'nine_frame_2st_frame_tft',
+        tff_key: 'nine_frame_2st_frame_tff',
+        ftt_key: 'nine_frame_2st_frame_ftt',
+        ftf_key: 'nine_frame_2st_frame_ftf',
+        fft_key: 'nine_frame_2st_frame_fft',
+        fff_key: 'nine_frame_2st_frame_fff'
+    },
+    nine_frame_3st_frame: {
+        ttt_key: 'nine_frame_3st_frame_ttt',
+        ttf_key: 'nine_frame_3st_frame_ttf',
+        tft_key: 'nine_frame_3st_frame_tft',
+        tff_key: 'nine_frame_3st_frame_tff',
+        ftt_key: 'nine_frame_3st_frame_ftt',
+        ftf_key: 'nine_frame_3st_frame_ftf',
+        fft_key: 'nine_frame_3st_frame_fft',
+        fff_key: 'nine_frame_3st_frame_fff'
+    },
+    nine_frame_4st_frame: {
+        ttt_key: 'nine_frame_4st_frame_ttt',
+        ttf_key: 'nine_frame_4st_frame_ttf',
+        tft_key: 'nine_frame_4st_frame_tft',
+        tff_key: 'nine_frame_4st_frame_tff',
+        ftt_key: 'nine_frame_4st_frame_ftt',
+        ftf_key: 'nine_frame_4st_frame_ftf',
+        fft_key: 'nine_frame_4st_frame_fft',
+        fff_key: 'nine_frame_4st_frame_fff'
+    },
+    nine_frame_5st_frame: {
+        ttt_key: 'nine_frame_5st_frame_ttt',
+        ttf_key: 'nine_frame_5st_frame_ttf',
+        tft_key: 'nine_frame_5st_frame_tft',
+        tff_key: 'nine_frame_5st_frame_tff',
+        ftt_key: 'nine_frame_5st_frame_ftt',
+        ftf_key: 'nine_frame_5st_frame_ftf',
+        fft_key: 'nine_frame_5st_frame_fft',
+        fff_key: 'nine_frame_5st_frame_fff'
+    },
+    nine_frame_6st_frame: {
+        ttt_key: 'nine_frame_6st_frame_ttt',
+        ttf_key: 'nine_frame_6st_frame_ttf',
+        tft_key: 'nine_frame_6st_frame_tft',
+        tff_key: 'nine_frame_6st_frame_tff',
+        ftt_key: 'nine_frame_6st_frame_ftt',
+        ftf_key: 'nine_frame_6st_frame_ftf',
+        fft_key: 'nine_frame_6st_frame_fft',
+        fff_key: 'nine_frame_6st_frame_fff'
+    },
+    nine_frame_7st_frame: {
+        ttt_key: 'nine_frame_7st_frame_ttt',
+        ttf_key: 'nine_frame_7st_frame_ttf',
+        tft_key: 'nine_frame_7st_frame_tft',
+        tff_key: 'nine_frame_7st_frame_tff',
+        ftt_key: 'nine_frame_7st_frame_ftt',
+        ftf_key: 'nine_frame_7st_frame_ftf',
+        fft_key: 'nine_frame_7st_frame_fft',
+        fff_key: 'nine_frame_7st_frame_fff'
+    },
+    nine_frame_8st_frame: {
+        ttt_key: 'nine_frame_8st_frame_ttt',
+        ttf_key: 'nine_frame_8st_frame_ttf',
+        tft_key: 'nine_frame_8st_frame_tft',
+        tff_key: 'nine_frame_8st_frame_tff',
+        ftt_key: 'nine_frame_8st_frame_ftt',
+        ftf_key: 'nine_frame_8st_frame_ftf',
+        fft_key: 'nine_frame_8st_frame_fft',
+        fff_key: 'nine_frame_8st_frame_fff'
+    },
+    nine_frame_9st_frame: {
+        ttt_key: 'nine_frame_9st_frame_ttt',
+        ttf_key: 'nine_frame_9st_frame_ttf',
+        tft_key: 'nine_frame_9st_frame_tft',
+        tff_key: 'nine_frame_9st_frame_tff',
+        ftt_key: 'nine_frame_9st_frame_ftt',
+        ftf_key: 'nine_frame_9st_frame_ftf',
+        fft_key: 'nine_frame_9st_frame_fft',
+        fff_key: 'nine_frame_9st_frame_fff'
+    }
 };
 $(document).ready(function(){
     $('#btnExact').off('click').on('click',btnExact_Click);
@@ -66,3 +140,58 @@ function append_trung_or_truot(is_trung){
         $('#lst_ket_qua').append(html_truot);    
     } 
 }
+function init_data() {
+    localStorage.setItem(global_key.nine_frame_1st_frame.ttt_key, JSON.stringify([true, true, true, true, true, true, true, true,true]));
+    localStorage.setItem(global_key.nine_frame_1st_frame.ttf_key, JSON.stringify([false, false, false, false, false, false, false, false,false]));
+    localStorage.setItem(global_key.nine_frame_1st_frame.tft_key, JSON.stringify([true, true, true, true, true, true, true, true,true]));
+    localStorage.setItem(global_key.nine_frame_1st_frame.tff_key, JSON.stringify([false, false, false, false, false, false, false, false,false]));
+    localStorage.setItem(global_key.nine_frame_1st_frame.ftt_key, JSON.stringify([true, true, true, true, true, true, true, true, true]));
+    localStorage.setItem(global_key.nine_frame_1st_frame.ftf_key, JSON.stringify([false, false, false, false, false, false, false, false,false]));
+    localStorage.setItem(global_key.nine_frame_1st_frame.fft_key, JSON.stringify([true, true, true, true, true, true, true, true,true]));
+    localStorage.setItem(global_key.nine_frame_1st_frame.fff_key, JSON.stringify([false, false, false, false, false, false, false, false,false]));
+
+    localStorage.setItem(global_key.nine_frame_2st_frame.ttt_key, JSON.stringify([true, false, true, false, true, false, true, false, true]));
+    localStorage.setItem(global_key.nine_frame_2st_frame.ttf_key, JSON.stringify([false, true, false, true, false, true, false, true, false]));
+    localStorage.setItem(global_key.nine_frame_2st_frame.tft_key, JSON.stringify([true, false, true, false, true, false, true, false, true]));
+    localStorage.setItem(global_key.nine_frame_2st_frame.tff_key, JSON.stringify([false, true, false, true, false, true, false, true, false]));
+    localStorage.setItem(global_key.nine_frame_2st_frame.ftt_key, JSON.stringify([true, false, true, false, true, false, true, false, true]));
+    localStorage.setItem(global_key.nine_frame_2st_frame.ftf_key, JSON.stringify([false, true, false, true, false, true, false, true, false]));
+    localStorage.setItem(global_key.nine_frame_2st_frame.fft_key, JSON.stringify([true, false, true, false, true, false, true, false, true]));
+    localStorage.setItem(global_key.nine_frame_2st_frame.fff_key, JSON.stringify([false, true, false, true, false, true, false, true, false]));
+
+    localStorage.setItem(global_key.nine_frame_3st_frame.ttt_key, JSON.stringify([true, true, true, false, false, false, true, true, true]));
+    localStorage.setItem(global_key.nine_frame_3st_frame.ttf_key, JSON.stringify([true, true, false, false, false, true, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_3st_frame.tft_key, JSON.stringify([false, false, true, true, true, false, false, false, true]));
+    localStorage.setItem(global_key.nine_frame_3st_frame.tff_key, JSON.stringify([true, false, false, false, true, true, true, false, false]));
+    localStorage.setItem(global_key.nine_frame_3st_frame.ftt_key, JSON.stringify([false, true, true, true, false, false, false, true, true]));
+    localStorage.setItem(global_key.nine_frame_3st_frame.ftf_key, JSON.stringify([true, true, false, false, false, true, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_3st_frame.fft_key, JSON.stringify([false, false, true, true, true, false, false, false, true]));
+    localStorage.setItem(global_key.nine_frame_3st_frame.fff_key, JSON.stringify([false, false, false, true, true, true, false, false, false]));
+
+    localStorage.setItem(global_key.nine_frame_4st_frame.ttt_key, JSON.stringify([true, false, true, true, false, true, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_4st_frame.ttf_key, JSON.stringify([false, true, false, false, true, false, false, false, true]));
+    localStorage.setItem(global_key.nine_frame_4st_frame.tft_key, JSON.stringify([true, false, true, true, false, true, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_4st_frame.tff_key, JSON.stringify([false, true, false, false, true, false, false, false, true]));
+    localStorage.setItem(global_key.nine_frame_4st_frame.ftt_key, JSON.stringify([true, false, true, true, false, true, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_4st_frame.ftf_key, JSON.stringify([false, true, false, false, true, false, false, false, true]));
+    localStorage.setItem(global_key.nine_frame_4st_frame.fft_key, JSON.stringify([true, false, true, true, false, true, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_4st_frame.fff_key, JSON.stringify([false, true, false, false, true, false, false, false, true]));
+
+    localStorage.setItem(global_key.nine_frame_5st_frame.ttt_key, JSON.stringify([true, true, false, false, true, true, false, false, true]));
+    localStorage.setItem(global_key.nine_frame_5st_frame.ttf_key, JSON.stringify([true, false, false, true, true, false, false, true, true]));
+    localStorage.setItem(global_key.nine_frame_5st_frame.tft_key, JSON.stringify([false, true, true, false, false, true, true, false, false]));
+    localStorage.setItem(global_key.nine_frame_5st_frame.tff_key, JSON.stringify([false, false, true, true, false, false, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_5st_frame.ftt_key, JSON.stringify([true, true, false, false, true, true, false, false, true]));
+    localStorage.setItem(global_key.nine_frame_5st_frame.ftf_key, JSON.stringify([true, false, false, true, true, false, false, true, true]));
+    localStorage.setItem(global_key.nine_frame_5st_frame.fft_key, JSON.stringify([false, true, true, false, false, true, true, false, false]));
+    localStorage.setItem(global_key.nine_frame_5st_frame.fff_key, JSON.stringify([false, false, true, true, false, false, true, true, false]));
+
+    localStorage.setItem(global_key.nine_frame_6st_frame.ttt_key, JSON.stringify([true, false, false, true, false, false, true, false, false]));
+    localStorage.setItem(global_key.nine_frame_6st_frame.ttf_key, JSON.stringify([false, false, true, false, false, true, false, false, true]));
+    localStorage.setItem(global_key.nine_frame_6st_frame.tft_key, JSON.stringify([true, true, false, true, true, false, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_6st_frame.tff_key, JSON.stringify([false, true, true, false, true, true, false, true, true]));
+    localStorage.setItem(global_key.nine_frame_6st_frame.ftt_key, JSON.stringify([true, false, false, true, false, false, true, false, false]));
+    localStorage.setItem(global_key.nine_frame_6st_frame.ftf_key, JSON.stringify([true, false, false, true, true, false, false, true, true]));
+    localStorage.setItem(global_key.nine_frame_6st_frame.fft_key, JSON.stringify([false, true, true, false, false, true, true, false, false]));
+    localStorage.setItem(global_key.nine_frame_6st_frame.fff_key, JSON.stringify([false, false, true, true, false, false, true, true, false]));
+} 
