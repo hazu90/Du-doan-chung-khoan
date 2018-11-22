9b2db83e5debed6a9f3d6550ce87cc56ce92b82b
hazu90 <hieubv.cntt@gmail.com>
2018-11-22 23:06:53 +0700

    commit from my home

--- Du doan tai chinh/content/js-ext/main-ext.js
+++ Du doan tai chinh/content/js-ext/main-ext.js
@@ -1,5 +1,6 @@
 var global_key ={
     nine_frame_key_du_doan: 'nine_frame_key_predict',
+    money :[1.05,3.21,7.64,16.74,35.41,74,153,315],
     nine_frame_1st_frame: {
         ttt_key: 'nine_frame_1st_frame_ttt',
         ttf_key: 'nine_frame_1st_frame_ttf',
@@ -8,7 +9,10 @@ var global_key ={
         ftt_key: 'nine_frame_1st_frame_ftt',
         ftf_key: 'nine_frame_1st_frame_ftf',
         fft_key: 'nine_frame_1st_frame_fft',
-        fff_key: 'nine_frame_1st_frame_fff'
+        fff_key: 'nine_frame_1st_frame_fff',
+        curr_predicts : 'nine_frame_1st_curr_predicts',
+        curr_index :'nine_frame_1st_curr_index',
+        order_view : '1st_frame'
     },
     nine_frame_2st_frame: {
         ttt_key: 'nine_frame_2st_frame_ttt',
@@ -18,7 +22,10 @@ var global_key ={
         ftt_key: 'nine_frame_2st_frame_ftt',
         ftf_key: 'nine_frame_2st_frame_ftf',
         fft_key: 'nine_frame_2st_frame_fft',
-        fff_key: 'nine_frame_2st_frame_fff'
+        fff_key: 'nine_frame_2st_frame_fff',
+        curr_predicts : 'nine_frame_2st_curr_predicts',
+        curr_index :'nine_frame_2st_curr_index',
+        order_view : '2st_frame'
     },
     nine_frame_3st_frame: {
         ttt_key: 'nine_frame_3st_frame_ttt',
@@ -28,7 +35,10 @@ var global_key ={
         ftt_key: 'nine_frame_3st_frame_ftt',
         ftf_key: 'nine_frame_3st_frame_ftf',
         fft_key: 'nine_frame_3st_frame_fft',
-        fff_key: 'nine_frame_3st_frame_fff'
+        fff_key: 'nine_frame_3st_frame_fff',
+        curr_predicts : 'nine_frame_3st_curr_predicts',
+        curr_index :'nine_frame_3st_curr_index',
+        order_view : '3st_frame'
     },
     nine_frame_4st_frame: {
         ttt_key: 'nine_frame_4st_frame_ttt',
@@ -38,7 +48,10 @@ var global_key ={
         ftt_key: 'nine_frame_4st_frame_ftt',
         ftf_key: 'nine_frame_4st_frame_ftf',
         fft_key: 'nine_frame_4st_frame_fft',
-        fff_key: 'nine_frame_4st_frame_fff'
+        fff_key: 'nine_frame_4st_frame_fff',
+        curr_predicts : 'nine_frame_4st_curr_predicts',
+        curr_index :'nine_frame_4st_curr_index',
+        order_view : '4st_frame'
     },
     nine_frame_5st_frame: {
         ttt_key: 'nine_frame_5st_frame_ttt',
@@ -48,7 +61,10 @@ var global_key ={
         ftt_key: 'nine_frame_5st_frame_ftt',
         ftf_key: 'nine_frame_5st_frame_ftf',
         fft_key: 'nine_frame_5st_frame_fft',
-        fff_key: 'nine_frame_5st_frame_fff'
+        fff_key: 'nine_frame_5st_frame_fff',
+        curr_predicts : 'nine_frame_5st_curr_predicts',
+        curr_index :'nine_frame_5st_curr_index',
+        order_view : '5st_frame'
     },
     nine_frame_6st_frame: {
         ttt_key: 'nine_frame_6st_frame_ttt',
@@ -58,7 +74,10 @@ var global_key ={
         ftt_key: 'nine_frame_6st_frame_ftt',
         ftf_key: 'nine_frame_6st_frame_ftf',
         fft_key: 'nine_frame_6st_frame_fft',
-        fff_key: 'nine_frame_6st_frame_fff'
+        fff_key: 'nine_frame_6st_frame_fff',
+        curr_predicts : 'nine_frame_6st_curr_predicts',
+        curr_index :'nine_frame_6st_curr_index',
+        order_view : '6st_frame'
     },
     nine_frame_7st_frame: {
         ttt_key: 'nine_frame_7st_frame_ttt',
@@ -68,7 +87,10 @@ var global_key ={
         ftt_key: 'nine_frame_7st_frame_ftt',
         ftf_key: 'nine_frame_7st_frame_ftf',
         fft_key: 'nine_frame_7st_frame_fft',
-        fff_key: 'nine_frame_7st_frame_fff'
+        fff_key: 'nine_frame_7st_frame_fff',
+        curr_predicts : 'nine_frame_7st_curr_predicts',
+        curr_index :'nine_frame_7st_curr_index',
+        order_view : '7st_frame'
     },
     nine_frame_8st_frame: {
         ttt_key: 'nine_frame_8st_frame_ttt',
@@ -78,7 +100,10 @@ var global_key ={
         ftt_key: 'nine_frame_8st_frame_ftt',
         ftf_key: 'nine_frame_8st_frame_ftf',
         fft_key: 'nine_frame_8st_frame_fft',
-        fff_key: 'nine_frame_8st_frame_fff'
+        fff_key: 'nine_frame_8st_frame_fff',
+        curr_predicts : 'nine_frame_8st_curr_predicts',
+        curr_index :'nine_frame_8st_curr_index',
+        order_view : '8st_frame'
     },
     nine_frame_9st_frame: {
         ttt_key: 'nine_frame_9st_frame_ttt',
@@ -88,7 +113,10 @@ var global_key ={
         ftt_key: 'nine_frame_9st_frame_ftt',
         ftf_key: 'nine_frame_9st_frame_ftf',
         fft_key: 'nine_frame_9st_frame_fft',
-        fff_key: 'nine_frame_9st_frame_fff'
+        fff_key: 'nine_frame_9st_frame_fff',
+        curr_predicts : 'nine_frame_9st_curr_predicts',
+        curr_index :'nine_frame_9st_curr_index',
+        order_view : '9st_frame'
     }
 };
 $(document).ready(function(){
@@ -97,7 +125,7 @@ $(document).ready(function(){
     $('#btnClear').off('click').on('click',function(){
         localStorage.clear();
         init_data();
-        init_page();
+        //init_page();
     });
     $('#btnRollback').off('click').on('click',function(){
         var obj= localStorage.getItem(global_key.key_du_doan);
@@ -120,16 +148,36 @@ $(document).ready(function(){
         }
         
     });
+
+    init_data();
 });
 
 function btnExact_Click(){
     append_trung_or_truot(true);
+    append_local_storage_trung_or_truot(true);
+    predict_next_steps(global_key.nine_frame_1st_frame);
+    predict_next_steps(global_key.nine_frame_2st_frame);
+    predict_next_steps(global_key.nine_frame_3st_frame);
+    predict_next_steps(global_key.nine_frame_4st_frame);
+    predict_next_steps(global_key.nine_frame_5st_frame);
+    predict_next_steps(global_key.nine_frame_6st_frame);
+    predict_next_steps(global_key.nine_frame_7st_frame);
+    predict_next_steps(global_key.nine_frame_8st_frame);
+    predict_next_steps(global_key.nine_frame_9st_frame);
 };
-
 function btnFail_Click(){
     append_trung_or_truot(false);
+    append_local_storage_trung_or_truot(false);
+    predict_next_steps(global_key.nine_frame_1st_frame);
+    predict_next_steps(global_key.nine_frame_2st_frame);
+    predict_next_steps(global_key.nine_frame_3st_frame);
+    predict_next_steps(global_key.nine_frame_4st_frame);
+    predict_next_steps(global_key.nine_frame_5st_frame);
+    predict_next_steps(global_key.nine_frame_6st_frame);
+    predict_next_steps(global_key.nine_frame_7st_frame);
+    predict_next_steps(global_key.nine_frame_8st_frame);
+    predict_next_steps(global_key.nine_frame_9st_frame);
 };
-
 function append_trung_or_truot(is_trung){
     var html_trung ='<div class="dudoan trung">T</div>';
     var html_truot = '<div class="dudoan truot">N</div>';
@@ -140,6 +188,161 @@ function append_trung_or_truot(is_trung){
         $('#lst_ket_qua').append(html_truot);    
     } 
 }
+function append_local_storage_trung_or_truot(is_trung){
+    var obj= localStorage.getItem(global_key.nine_frame_key_du_doan);
+    var arr_du_doan =[];
+    if(obj != null){
+        arr_du_doan = JSON.parse(obj);   
+    }    
+    arr_du_doan.push(is_trung);
+    localStorage.setItem(global_key.nine_frame_key_du_doan,JSON.stringify(arr_du_doan) );    
+}
+function predict_next_steps (order_frame_obj){
+    var obj_du_doan = localStorage.getItem(global_key.nine_frame_key_du_doan);
+    var arr_du_doan =[];
+    if(obj_du_doan != null){
+        arr_du_doan = JSON.parse(obj_du_doan);
+    }
+    if(arr_du_doan.length <3){
+        return;
+    }
+    // Lấy ra kết quả đã dự đoán trước đó
+    var obj_before_du_doan = localStorage.getItem(global_key.nine_frame_1st_frame.curr_predicts);
+    var index_before_du_doan = localStorage.getItem(global_key.nine_frame_1st_frame.curr_index);
+    if (index_before_du_doan == null) {
+        index_before_du_doan = 0;
+    }
+    else {
+        index_before_du_doan = parseInt(index_before_du_doan);
+    } 
+    var arr_before_du_doan =[];
+    var is_repeat_du_doan = true;
+    if(obj_before_du_doan != null){
+        arr_before_du_doan = JSON.parse(obj_before_du_doan);
+        if (index_before_du_doan < 7 && arr_before_du_doan.length > index_before_du_doan){
+            if(arr_before_du_doan[index_before_du_doan ] != arr_du_doan[arr_du_doan.length -1]){
+                is_repeat_du_doan = false;
+            }    
+        }
+    }
+
+    // Lấy ra 3 kết quả gần nhất
+    var key = '';
+
+    if(is_repeat_du_doan){
+
+        key = get_key_predict(arr_du_doan,order_frame_obj);
+
+        var arr_predict =[];
+        // Lấy ra kết quả dự đoán
+        var obj_predict = localStorage.getItem(key);
+        if(obj_predict != null){
+            arr_predict = JSON.parse(obj_predict);
+        }
+        
+        show_predict_ext(order_frame_obj.order_view,arr_predict,0,true);
+        localStorage.setItem(order_frame_obj.curr_predicts,JSON.stringify(arr_predict) );
+        localStorage.setItem(order_frame_obj.curr_index, 0);
+    }
+    else{
+        // Tăng lên 1 mức đầu tư
+        index_before_du_doan = index_before_du_doan+1;
+        show_predict_ext(order_frame_obj.order_view,arr_predict,0,true);
+        localStorage.setItem(global_key.curr_predict_continous,JSON.stringify(arr_before_du_doan) );
+        localStorage.setItem(global_key.curr_index_continous,index_before_du_doan);
+    }
+}
+
+function get_key_predict(arr_du_doan,order_frame){
+    var lght_arr_du_doan = arr_du_doan.length;
+    var key = '';
+    if(arr_du_doan[lght_arr_du_doan- 3]){
+        key += 't';
+    }
+    else{
+        key += 'f';
+    }
+
+    if(arr_du_doan[lght_arr_du_doan- 2]){
+        key += 't';
+    }
+    else{
+        key += 'f';
+    }
+
+    if(arr_du_doan[lght_arr_du_doan- 1]){
+        key += 't';
+    }
+    else{
+        key += 'f';
+    }
+
+    switch(key){
+        case 'ttt' : return order_frame.ttt_key; break;
+        case 'ttf' : return order_frame.ttf_key; break;
+        case 'tft' : return order_frame.tft_key; break;
+        case 'tff' : return order_frame.tff_key; break;
+        case 'ftt' : return order_frame.ftt_key; break;
+        case 'ftf' : return order_frame.ftf_key; break;
+        case 'fft' : return order_frame.fft_key; break;
+        case 'fff' : return order_frame.fff_key; break;
+    }
+}
+
+function show_predict_ext(order_view,arr_predict,suggest_chose,is_calc_predict){
+    if(arr_predict.length ==0){
+        $('#result_predict_cap_lien_tiep').html('');
+        $('label[name=money_predict_cap_lien_tiep]').html('Số tiền : 0');
+        $('label[name=money_predict_cap_lien_tiep]').data('typemoney', 'N');
+        $('label[name=money_predict_cap_lien_tiep]').data('amountmoney', 0);
+        return;
+    }
+
+    var type_money = '';
+    var html_predict='';
+    for(var index =0;index < arr_predict.length;index++){
+        if(arr_predict[index]){
+            html_predict += '<div class="predict-choosen">';
+            html_predict += '   <div class="arrow-up"></div>';
+            if (index == suggest_chose && is_calc_predict) {
+                type_money = 'T';
+                html_predict += '<label style="border-bottom: 2px solid red;">T'+(index+1)+'</label>';
+            }
+            else{
+                html_predict += '   T'+(index+1);
+            }
+            
+            html_predict += '</div>';
+        }
+        else{
+            html_predict += '<div class="predict-choosen">';
+            html_predict += '   <div class="arrow-down"></div>';
+            if (index == suggest_chose && is_calc_predict) {
+                type_money = 'N';
+                html_predict += '<label style="border-bottom: 2px solid red;">N'+(index+1)+'</label>';
+            }
+            else{
+                html_predict += '   N'+(index+1);
+            }
+            
+            html_predict += '</div>';
+        }
+    }
+    $('#result_predict_'+order_view).html(html_predict);
+    // if(is_calc_predict){
+    //     type_money = predict_lien_tiep.get_predict_money_type_compare_latest_step(type_money);
+    //     $('label[name=money_predict_cap_lien_tiep]').html('Số tiền : ' + type_money  + global_key.money[suggest_chose]);
+    //     $('label[name=money_predict_cap_lien_tiep]').data('typemoney', type_money);
+    //     $('label[name=money_predict_cap_lien_tiep]').data('amountmoney', global_key.money[suggest_chose]);
+    // }
+    // else{
+    //     $('label[name=money_predict_cap_lien_tiep]').html('Số tiền : 0');
+    //     $('label[name=money_predict_cap_lien_tiep]').data('typemoney', 'N');
+    //     $('label[name=money_predict_cap_lien_tiep]').data('amountmoney', 0);
+    // }
+    //general_lib.calc_total(type_money, global_key.money[suggest_chose]);
+}
+
 function init_data() {
     localStorage.setItem(global_key.nine_frame_1st_frame.ttt_key, JSON.stringify([true, true, true, true, true, true, true, true,true]));
     localStorage.setItem(global_key.nine_frame_1st_frame.ttf_key, JSON.stringify([false, false, false, false, false, false, false, false,false]));
@@ -191,7 +394,34 @@ function init_data() {
     localStorage.setItem(global_key.nine_frame_6st_frame.tft_key, JSON.stringify([true, true, false, true, true, false, true, true, false]));
     localStorage.setItem(global_key.nine_frame_6st_frame.tff_key, JSON.stringify([false, true, true, false, true, true, false, true, true]));
     localStorage.setItem(global_key.nine_frame_6st_frame.ftt_key, JSON.stringify([true, false, false, true, false, false, true, false, false]));
-    localStorage.setItem(global_key.nine_frame_6st_frame.ftf_key, JSON.stringify([true, false, false, true, true, false, false, true, true]));
-    localStorage.setItem(global_key.nine_frame_6st_frame.fft_key, JSON.stringify([false, true, true, false, false, true, true, false, false]));
-    localStorage.setItem(global_key.nine_frame_6st_frame.fff_key, JSON.stringify([false, false, true, true, false, false, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_6st_frame.ftf_key, JSON.stringify([false, false, true, false, false, true, false, false, true]));
+    localStorage.setItem(global_key.nine_frame_6st_frame.fft_key, JSON.stringify([true, true, false, true, true, false, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_6st_frame.fff_key, JSON.stringify([false, true, true, false, true, true, false, true, true]));
+
+    localStorage.setItem(global_key.nine_frame_7st_frame.ttt_key, JSON.stringify([true, true, true, false, true, true, false, true, false]));
+    localStorage.setItem(global_key.nine_frame_7st_frame.ttf_key, JSON.stringify([false, false, false, true, false, false, true, false, true]));
+    localStorage.setItem(global_key.nine_frame_7st_frame.tft_key, JSON.stringify([true, true, true, false, true, true, false, true, false]));
+    localStorage.setItem(global_key.nine_frame_7st_frame.tff_key, JSON.stringify([false, false, false, true, false, false, true, false, true]));
+    localStorage.setItem(global_key.nine_frame_7st_frame.ftt_key, JSON.stringify([true, true, true, false, true, true, false, true, false]));
+    localStorage.setItem(global_key.nine_frame_7st_frame.ftf_key, JSON.stringify([false, false, false, true, false, false, true, false, true]));
+    localStorage.setItem(global_key.nine_frame_7st_frame.fft_key, JSON.stringify([true, true, true, false, true, true, false, true, false]));
+    localStorage.setItem(global_key.nine_frame_7st_frame.fff_key, JSON.stringify([false, false, false, true, false, false, true, false, true]));
+
+    localStorage.setItem(global_key.nine_frame_8st_frame.ttt_key, JSON.stringify([true, false, false, false, true, false, false, false, true]));
+    localStorage.setItem(global_key.nine_frame_8st_frame.ttf_key, JSON.stringify([false, false, false, true, false, false, false, true, false]));
+    localStorage.setItem(global_key.nine_frame_8st_frame.tft_key, JSON.stringify([false, false, true, false, false, false, true, false, false]));
+    localStorage.setItem(global_key.nine_frame_8st_frame.tff_key, JSON.stringify([false, true, true, true, false, true, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_8st_frame.ftt_key, JSON.stringify([true, false, false, false, true, false, false, false, true]));
+    localStorage.setItem(global_key.nine_frame_8st_frame.ftf_key, JSON.stringify([true, true, false, true, true, true, false, true, true]));
+    localStorage.setItem(global_key.nine_frame_8st_frame.fft_key, JSON.stringify([true, true, true, false, true, true, true, false, true]));
+    localStorage.setItem(global_key.nine_frame_8st_frame.fff_key, JSON.stringify([false, true, true, true, false, true, true, true, false]));
+
+    localStorage.setItem(global_key.nine_frame_9st_frame.ttt_key, JSON.stringify([true, true, false, true, true, false, true, true, false]));
+    localStorage.setItem(global_key.nine_frame_9st_frame.ttf_key, JSON.stringify([true, false, true, true, false, true, true, false, true]));
+    localStorage.setItem(global_key.nine_frame_9st_frame.tft_key, JSON.stringify([false, true, false, false, true, false, false, true, false]));
+    localStorage.setItem(global_key.nine_frame_9st_frame.tff_key, JSON.stringify([false, true, true, false, true, true, false, true, true]));
+    localStorage.setItem(global_key.nine_frame_9st_frame.ftt_key, JSON.stringify([true, false, false, true, false, false, true, false, false]));
+    localStorage.setItem(global_key.nine_frame_9st_frame.ftf_key, JSON.stringify([true, false, true, true, false, true, true, false, true]));
+    localStorage.setItem(global_key.nine_frame_9st_frame.fft_key, JSON.stringify([false, true, false, false, true, false, false, true, false]));
+    localStorage.setItem(global_key.nine_frame_9st_frame.fff_key, JSON.stringify([false, false, true, false, false, true, false, false, true]));
 } 
